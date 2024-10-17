2ccb116060d5d2be741a0d66b6da731576f97f3c
Richard Li <lichard49@gmail.com>
2024-10-16 23:09:35 -0700

    Added recording functionality

--- plot_and_record.py
+++ plot_and_record.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pyqtgraph as pg
+from pyqtgraph.Qt import QtWidgets
 import serial
+import time
 
 
 if __name__ == '__main__':
@@ -12,6 +14,11 @@ if __name__ == '__main__':
   NUM_CHANNELS = 3
   # number of samples to plot per axes
   PLOT_WIDTH = 300
+  # number of labels to record for
+  LABELS = [
+    'wave',
+    'punch'
+  ]
 
   # set up serial port and data structure
   serial_port = serial.Serial(SERIAL_PORT_NAME, SERIAL_PORT_BAUD_RATE)
@@ -25,6 +32,42 @@ if __name__ == '__main__':
     for i in range(NUM_CHANNELS)
   ]
 
+  # set up callback for handling record button clicks
+  is_recording = False
+  current_recording_file = None
+
+  def onButtonClicked(label):
+    global is_recording, current_recording_file
+
+    # toggle recording state
+    is_recording = not is_recording
+
+    if is_recording:
+      # update background to reflect recording state
+      window.setBackground('red')
+
+      # set up file writing
+      # include timestamp so data does not accidentally get overwritten
+      recording_file_name =  label + '_' + str(time.time()) + '.txt'
+      recording_file_path = 'data/' + recording_file_name
+      current_recording_file = open(recording_file_path, 'w')
+    else:
+      window.setBackground('black')
+      current_recording_file.close()
+
+  # set up buttons that start recording when clicked
+  button_row = window.addLayout(row=4, col=1)
+
+  for i in range(len(LABELS)):
+    label = LABELS[i]
+    button = QtWidgets.QPushButton(label)
+    button_callback = lambda _, label=label: onButtonClicked(label)
+    button.clicked.connect(button_callback)
+
+    proxy = QtWidgets.QGraphicsProxyWidget()
+    proxy.setWidget(button)
+    button_row.addItem(proxy)
+
   # update all plots
   def update():
     global data
@@ -34,18 +77,22 @@ if __name__ == '__main__':
     # convert to a string
     data_line = data_line.decode('utf-8')
     # remove trailing whitespace
-    data_line = data_line.strip()
-    print(data_line)
+    parsed_data_line = data_line.strip()
+    # print(parsed_data_line)
     # parse assuming a tab character delimiter
-    data_line = data_line.split('\t')
+    parsed_data_line = parsed_data_line.split('\t')
 
     # if the line read from serial port is the correct format
-    if len(data_line) == NUM_CHANNELS:
+    if len(parsed_data_line) == NUM_CHANNELS:
+      if is_recording and current_recording_file is not None:
+        # write to file when recording
+        current_recording_file.write(data_line)
+
       for i in range(NUM_CHANNELS):
         # slide window by popping first sample...
         data[i, :-1] = data[i, 1:]
         # ...and appending new sample
-        data[i, -1] = float(data_line[i])
+        data[i, -1] = float(parsed_data_line[i])
 
         # update plot with new sample included
         plots[i].setData(data[i])
